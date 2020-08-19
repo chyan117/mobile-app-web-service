98dc3e3eee5f906fcf94060fa3d4ad201fe1934d
Hsin-Chien Chang <kingchang33@gmail.com>
2020-08-19 12:11:12 +0800

    first commit

---
+++ src/test/java/springframework/mobile/app/mobileappws/MobileAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.mobile.app.mobileappws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
